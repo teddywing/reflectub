f4a44010a7dc0ad8c5ec236de0310b4f4913daac
Teddy Wing <git@teddywing.com>
2021-05-29 14:09:36 +0200

    Make a test request to the GitHub API to get user repos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use reqwest::blocking::Client;
+
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+
+    let response = client.request(
+        reqwest::Method::GET,
+        format!(
+            "https://api.github.com/users/{}/repos",
+            "teddywing",
+        ),
+    )
+        .header("Accept", "application/vnd.github.v3+json")
+        .header("User-Agent", "reflectub")
+        .send()
+        .unwrap();
+
+    dbg!(&response);
 }
